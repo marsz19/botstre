92fecfcce69b2a19090e01bd2ea6baf7cb0bbbd4
marsz19 <marselstokakun@gmail..com>
2022-07-29 06:24:05 +0700

    botstre

---
+++ help.js
@@ -0,0 +1,47 @@
+exports.menu = (ucapan, pushName, ownerName, botName, tanggal, jam, runtime, prefix) => {
+    return `${ucapan} ${pushName}
+    
+                      $ *MAU SEWA BOT?KETIK LIST*
+ 
+👑 Creator : ${ownerName}
+🤖 Bot Name : ${botName}
+📆 Tanggal : ${tanggal}
+⌚ Jam : ${jam}
+⏳ Runtime
+${runtime}
+
+• ${prefix}sticker
+• ${prefix}swm *text | text*
+• ${prefix}toimg
+• ${prefix}kick @tag
+• ${prefix}add 62855xxx
+• ${prefix}group *open/close*
+• ${prefix}setgcname *text*
+• ${prefix}setgcdesk *text*
+• ${prefix}hidetag *text*
+• ${prefix}antilink *on/off*
+• ${prefix}leave
+• ${prefix}setexif *text | text*
+• ${prefix}addlist *key@response*
+• ${prefix}dellist *key*
+• ${prefix}updatelist *key@response*
+• ${prefix}resetlist
+• ${prefix}list
+• ${prefix}p
+• ${prefix}setp
+• ${prefix}changep
+• ${prefix}delsetp
+• ${prefix}d
+• ${prefix}setd
+• ${prefix}changed
+• ${prefix}delsetd
+•                     $ *MAU SEWA BOT?KETIK LIST*
+
+*EXECUTE*
+• $
+• >
+• =>
+`
+}
+
+
